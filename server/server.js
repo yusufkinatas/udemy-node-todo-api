253c58088c1ecffcc1ea7dbe62cfe69779ec3129
Yusuf <yusufkinatas@gmail.com>
2019-01-11 20:14:55 +0300

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post("/todos", (req, res) => {
-  
+
   var todo = new Todo({
     text: req.body.text
   });
@@ -19,6 +19,14 @@ app.post("/todos", (req, res) => {
   }, err => {
     res.status(400).send(err);
   });
+});
+
+app.get("/todos", (req, res) => {
+  Todo.find().then(todos => {
+    res.send({ todos });
+  }, err => {
+    res.status(400).send(err);
+  })
 });
 
 app.listen(3000, () => {
