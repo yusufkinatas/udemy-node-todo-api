70f4247719670ff91c79ecfec45dbf63e2f3c7b1
Yusuf <yusufkinatas@gmail.com>
2019-01-12 11:54:00 +0300

    add test DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -102,3 +102,40 @@ describe("GET /todos/:id", () => {
 
 
 });
+
+describe("DELETE /todos/:id", () => {
+  it("should remove a todo", (done) => {
+    var tempID = todos[0]._id.toHexString();
+    request(app)
+      .delete(`/todos/${tempID}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo._id).toBe(tempID);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        Todo.findById(tempID).then(todo => {
+          expect(todo).toNotExist();
+          done();
+        }).catch(err => done(err));
+      });
+  });
+
+  it("should return 404 if todo not found", (done) => {
+    var tempID = new ObjectID().toHexString();
+    request(app)
+      .delete(`/todos/${tempID}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it("should return 404 if object id is invalid", (done) => {
+    request(app)
+      .delete("/todos/123")
+      .expect(404)
+      .end(done);
+  });
+
+})
