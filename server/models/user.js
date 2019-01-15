7536861f62f9c7988c94aaf9a74d042232ea0d3e
Yusuf <yusufkinatas@gmail.com>
2019-01-15 10:16:00 +0300

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -44,7 +44,7 @@ UserSchema.pre("save", function (next) {
         user.password = hash;
         next();
       })
-    })
+    });
   }
   else {
     next();
@@ -89,6 +89,28 @@ UserSchema.statics.findByToken = function (token) {
     "tokens.access": "auth"
   });
 
+};
+
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({ email }).then(user => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        }
+        else {
+          reject();
+        }
+      })
+    });
+
+  });
 };
 
 var User = mongoose.model("User", UserSchema);
--- server/server.js
+++ server/server.js
@@ -122,6 +122,18 @@ app.get("/users/me", authenticate, (req, res) => {
 
 });
 
+app.post("/users/login", (req, res) => {
+  var body = _.pick(req.body, ["email", "password"]);
+
+  User.findByCredentials(body.email, body.password).then(user => {
+    user.generateAuthToken().then(token => {
+      res.header("x-auth", token).send(user);
+    });
+  }).catch(err => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
   console.log("Started on port", port);
 });
