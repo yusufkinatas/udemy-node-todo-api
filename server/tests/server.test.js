acfd0a18b21484300b47ccead99ebd20dc0ea033
Yusuf <yusufkinatas@gmail.com>
2019-01-15 11:02:12 +0300

    Add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -9,7 +9,7 @@ const { todos, populateTodos, users, populateUsers } = require("./seed/seed");
 
 beforeEach(populateUsers);
 beforeEach(populateTodos);
-
+/*
 describe('POST /todos', () => {
   it("should create a new todo", (done) => {
     var text = "Test todo text";
@@ -209,7 +209,7 @@ describe("POST /users/", () => {
           expect(user).toExist();
           expect(user.password).toNotBe(password);
           done();
-        });
+        }).catch(err => done(err));
 
       });
 
@@ -234,6 +234,56 @@ describe("POST /users/", () => {
       .expect(400)
       .end(done);
 
+  });
+
+});
+*/
+
+describe("POST /users/login", () => {
+
+  it("should login user and return auth token", (done) => {
+    request(app)
+      .post("/users/login")
+      .send({ email: users[1].email, password: users[1].password })
+      .expect(200)
+      .expect(res => {
+        expect(res.headers["x-auth"]).toExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then(user => {
+          expect(user.tokens[0]).toInclude({
+            access: "auth",
+            token: res.headers["x-auth"]
+          });
+          done();
+        }).catch(err => done(err));
+
+      });
+  });
+
+  it("should reject invalid login", (done) => {
+    request(app)
+      .post("/users/login")
+      .send({ email: users[1].email, password: users[1].password + 1 })
+      .expect(400)
+      .expect(res => {
+        expect(res.headers["x-auth"]).toNotExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then(user => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch(err => done(err));
+
+      });
   });
 
 });
